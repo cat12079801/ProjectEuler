6a9e7dcef7b0d447d41fc8f939340f05acc6373f
cat <cat12079801@gmail.com>
2013-08-09 14:57:31 +0900

    solved Problem1 with c

--- Problem1.c
+++ Problem1.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+int main(){
+	int i, sum = 0;
+
+	for(i = 3 ; i < 1000 ; i +=3){
+		sum += i;
+	}
+	for(i = 5 ; i < 1000 ; i += 5){
+		if(i % 3 != 0)
+			sum += i;
+	}
+	printf("sum=%d\n", sum);
+
+	return 0;
+}
