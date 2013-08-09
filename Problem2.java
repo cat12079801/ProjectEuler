7d76bbda98716e078d4cbc3ebb3b7eb3f59bf540
cat <cat12079801@gmail.com>
2013-08-09 15:21:54 +0900

    solved Problem2 with Java

---
+++ Problem2.java
@@ -0,0 +1,13 @@
+public class Problem2{
+	public static void main(String[] args){
+		int a = 1, b = 2, c, sum;
+
+		sum = 2;
+		for(c = a + b ; c < 4000000 ; a = b, b = c){
+			if(c % 2 == 0)
+				sum += c;
+			c = a + b;
+		}
+		System.out.println("sum=" + sum);
+	}
+}
