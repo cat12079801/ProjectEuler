922368657c9238f5d78d5004a9e1d7892c6b9a14
cat <cat12079801@gmail.com>
2013-08-14 22:16:29 +0900

    solved Problem4 with Java

---
+++ Problem4.java
@@ -0,0 +1,18 @@
+public class Problem4{
+	public static void main(String[] args){
+		int i, j, target, answer = 0;
+		String s, rev;
+
+		for(i = 100 ; i < 1000 ; i++){
+			for(j = 100 ; j <1000 ; j++){
+				target = i * j;
+				s = String.valueOf(target);
+				StringBuffer sb = new StringBuffer(s);
+				rev = sb.reverse().toString();
+				if(s.equals(rev) && target > answer)
+					answer = target;
+			}
+		}
+		System.out.println("Answer=" + answer);
+	}
+}
